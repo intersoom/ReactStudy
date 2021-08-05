91fe727b7e940d77e11ba951810d6d270556fc36
intersoom <sallybig@naver.com>
2021-08-06 00:57:53 +0900

    #02 섹션 2 끝

--- src/App.js
+++ src/App.js
@@ -1,11 +1,26 @@
+import React, {Component} from 'react';
+import TOC from './components/TOC'
+import Content from './components/Content'
+import Subject from './components/Subject'
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      Hello React!!
-    </div>
-  );
+
+
+
+
+
+
+//유사 javascript (찐 아님!!) -> jsx임!
+class App extends Component{ //component라는 class를 상속 받아서 App이라는 class를 만듦
+  render(){ // 해당 class는 render()라는 method를 갖고 있음
+    return(
+      <div>
+        <Subject title="WEB" sub="world wide web!"></Subject>
+        <TOC />
+        <Content title="HTML" sub="HTML is HyperText Markup Language."></Content>
+      </div>
+    )
+  }
 }
 
 export default App;
